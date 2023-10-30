de2ac5c60dddf206bea4bf35462c7277f58eb479
Suzuki Takehiro <beni.web.jp@gmail.com>
2023-10-30 09:45:53 +0900

    provider version up to 5.23.0

--- terraform/envs/main_region/version.tf
+++ terraform/envs/main_region/version.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0.0"
+      version = "~> 5.23.0"
     }
   }
   required_version = ">= 1.6.0"
