50c5dbf36fbcc5c215702b9389f382f70b79ac9d
Suzuki Takehiro <beni.web.jp@gmail.com>
2023-10-31 11:50:27 +0900

    changed a variable setting

---
+++ terraform/README.md
@@ -0,0 +1,6 @@
+# ファイル設計
+下記のtfファイルを構成ファイルとし、その他のtfファイルをサービス定義ファイルとする。
+  
++ version.tf
++ provider.tf
++ outputs.tf
--- terraform/envs/main_region/network.tf
+++ terraform/envs/main_region/network.tf
@@ -1,9 +1,9 @@
 # -----------------------------------
 # VPC
 # -----------------------------------
-resource "aws_vpc" "prod-vpc" {
+resource "aws_vpc" "prod_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "prod-vpc"
+    Name = "prod_vpc"
   }
 }
