ac9d20e1628fb521a853eca89c852e68e056c125
Suzuki Takehiro <beni.web.jp@gmail.com>
2023-11-04 12:17:12 +0900

    created private_admin_bucket

--- terraform/envs/main_region/s3.tf
+++ terraform/envs/main_region/s3.tf
@@ -1,6 +1,7 @@
 # -----------------------------------
 # S3
 # -----------------------------------
+# TODO s3 cross region replicationの実装。
 # ランダムID生成
 # "random_string"の使用は公式非推奨
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
@@ -11,6 +12,7 @@ resource "random_id" "s3" {
   }
 }
 # public bucket
+# TODO 静的ウェブサイトのホスティングをALBを実装するタイミングで実装する。
 resource "aws_s3_bucket" "public_assets" { # versioningをこの中で使用する事は公式非推奨。そのため、"aws_s3_bucket_versioning"内で実装。
   bucket = format("%s-public-assets-%s", var.env_name, random_id.s3.hex)
   tags = {
@@ -42,7 +44,8 @@ resource "aws_s3_bucket_policy" "public_assets_policy" {
 }
 data "aws_iam_policy_document" "allow_access_from_public" {
   statement {
-    principals { # 公式より右記を抜粋。"To have Terraform render JSON containing "Principal": {"AWS": "*"}, use type = "AWS" and identifiers = ["*"]."
+    effect = "Allow" # デフォルトは"Allow"
+    principals {     # 公式より右記を抜粋。"To have Terraform render JSON containing "Principal": {"AWS": "*"}, use type = "AWS" and identifiers = ["*"]."
       type        = "AWS"
       identifiers = ["*"]
     }
@@ -50,8 +53,8 @@ data "aws_iam_policy_document" "allow_access_from_public" {
       "S3:GetObject",
     ]
     resources = [
-      aws_s3_bucket.public_assets.arn,
-      "${aws_s3_bucket.public_assets.arn}/*"
+      aws_s3_bucket.public_assets.arn,       # buckerそのものへのアクセス
+      "${aws_s3_bucket.public_assets.arn}/*" # bucket内のオブジェクトへのアクセス
     ]
   }
 }
@@ -76,46 +79,70 @@ resource "aws_s3_bucket_cors_configuration" "public_assets_cors" {
 */
 
 
-# private bucket
-# loopを防ぐ実装を行う
-/*
-https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-上記方法でも暗号化ができる。どちらにするか検討する。上の方法だとserver_side_encryption 属性を付与している。ただ、S3はデフォルトでも暗号化されている
-resource "aws_s3_bucket_server_side_encryption_configuration" "private_admin" {
-  bucket = aws_s3_bucket.private_admin.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
+# private bucket for admin use
+resource "aws_s3_bucket" "private_admin" { # versioningをこの中で使用する事は公式非推奨。そのため、"aws_s3_bucket_versioning"内で実装。
+  bucket = format("%s-private-admin-%s", var.env_name, random_id.s3.hex)
+  tags = {
+    Name = format("%s-private-admin-%s", var.env_name, random_id.s3.hex)
   }
 }
-resource "aws_s3_bucket_public_access_block" "private_ab" {
-  bucket                  = aws_s3_bucket.private_ab.id
+resource "aws_s3_bucket_versioning" "private_admin_versioning" {
+  bucket = aws_s3_bucket.private_admin.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_public_access_block" "private_admin_ab" {
+  bucket                  = aws_s3_bucket.private_admin.id
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false # S3バケットを自動作成する処理の中で、block_public_policyはデフォルトtrueなのでfalseにしないと新規作成時でも設定したポリシーの適応が失敗し構築が失敗する。
   ignore_public_acls      = true
   restrict_public_buckets = true
   depends_on = [
-    aws_s3_bucket_policy.public_assets_policy
+    aws_s3_bucket.private_admin
   ]
 }
-resource "aws_s3_object" "object" {
-  bucket = aws_s3_bucket.public_assets.id
-  key    = "new_object_key"
-  source = "path/to/file"
-  etag = filemd5("path/to/file") # privateの場合、etagの使用を検討する
+resource "aws_s3_bucket_policy" "private_admin_policy" {
+  bucket = aws_s3_bucket.private_admin.id
+  policy = data.aws_iam_policy_document.limited_access_only_private_admin.json
+  depends_on = [
+    aws_s3_bucket_public_access_block.private_admin_ab
+  ]
 }
-resource "aws_s3_bucket_lifecycle_configuration" "public_assets" {
-  bucket = aws_s3_bucket.public_assets.id
+data "aws_iam_policy_document" "limited_access_only_private_admin" {
+  statement {
+    effect = "Allow" # デフォルトは"Allow"
+    principals {     # 特定のadminユーザを許可。
+      type        = "AWS"
+      identifiers = [var.admin_iam_arn]
+    }
+    actions = [
+      "S3:*",
+    ]
+    resources = [
+      aws_s3_bucket.private_admin.arn,       # buckerそのものへのアクセス
+      "${aws_s3_bucket.private_admin.arn}/*" # bucket内のオブジェクトへのアクセス
+    ]
+  }
+}
+# private bucket for system logs use
+
+
+
+/*
+resource "aws_s3_bucket_lifecycle_configuration" "private_sys_logs_lifecycle" {
+  bucket = aws_s3_bucket.private_sys_logs.id
   rule {
-    id     = "public_assets"
+    id     = "private_sys_logs"
     status = "Enabled"
     expiration {
       days = 90
     }
-    filter { # prefixを指定しない場合必須。(ここでいうprefixとは、filter内prefixではなく、filterと同列に配置できるprefix。)lifecycle ruleを適応する適用する範囲を指定。
+    filter { # prefixを指定しない場合必須。(ここでいうprefixとは、filter内prefixではなく、filterと同列に配置できるprefix。)lifecycle ruleを適応する適用する範囲を指定。ここでは全てを指定。
       prefix = ""
     }
   }
 }
 */
+# private bucket for system logs loop avoidance use
+# TODO loopを防ぐ実装を必要になったタイミングで行う
--- terraform/envs/main_region/variables.tf
+++ terraform/envs/main_region/variables.tf
@@ -6,3 +6,7 @@ variable "env_name" {
   description = "a environment name"
   type        = string
 }
+variable "admin_iam_arn" {
+  description = "a admin iam arn"
+  type        = string
+}
