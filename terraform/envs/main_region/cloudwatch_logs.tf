1fc8306ef65f8d8569568ede46b107b63092ab0a
Suzuki Takehiro <beni.web.jp@gmail.com>
2023-11-07 14:28:13 +0900

    modified paths of cloudwatch_logs

--- terraform/envs/main_region/cloudwatch_logs.tf
+++ terraform/envs/main_region/cloudwatch_logs.tf
@@ -2,14 +2,14 @@
 # CloudWatch Logs
 # -----------------------------------
 resource "aws_cloudwatch_log_group" "error" {
-  name              = format("/%s/rds/error", var.env_name)
+  name              = format("/aws/rds/instance/%s-rds-standalone/error", var.env_name)
   retention_in_days = 90
 }
 resource "aws_cloudwatch_log_group" "general" {
-  name              = format("/%s/rds/general", var.env_name)
+  name              = format("/aws/rds/instance/%s-rds-standalone/general", var.env_name)
   retention_in_days = 90
 }
 resource "aws_cloudwatch_log_group" "slowquery" {
-  name              = format("/%s/rds/slowquery", var.env_name)
+  name              = format("/aws/rds/instance/%s-rds-standalone/slowquery", var.env_name)
   retention_in_days = 90
 }
