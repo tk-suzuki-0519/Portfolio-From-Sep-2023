7c73f15aef938e2786f9219946ccee54c75b3996
Suzuki Takehiro <beni.web.jp@gmail.com>
2023-11-08 12:18:05 +0900

    fixed ip_address_type error

--- terraform/envs/main_region/vpc_endpoint.tf
+++ terraform/envs/main_region/vpc_endpoint.tf
@@ -7,7 +7,6 @@ resource "aws_vpc_endpoint" "s3" {
   service_name      = format("com.amazonaws.%s.s3", var.main_region)
   vpc_endpoint_type = "Gateway"
   route_table_ids   = [aws_route_table.private_app_rt.id]
-  ip_address_type   = "ipv4"
   tags = {
     Name = format("%s_endpoint_s3", var.env_name)
   }
