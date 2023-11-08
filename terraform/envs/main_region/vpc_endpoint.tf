c2fb19b51cca0302a3a4ebbf2ecf73cb799f7f11
Suzuki Takehiro <beni.web.jp@gmail.com>
2023-11-08 11:51:46 +0900

    S3 gateway endpoint

--- terraform/envs/main_region/vpc_endpoint.tf
+++ terraform/envs/main_region/vpc_endpoint.tf
@@ -2,18 +2,15 @@
 # VPC endpoint
 # -----------------------------------
 # VPC endpoint(Gateway) for S3
-
-
-
-
-
-
-
-
-
-
-
-
-
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.vpc.id
+  service_name      = format("com.amazonaws.%s.s3", var.main_region)
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private_app_rt.id]
+  ip_address_type   = "ipv4"
+  tags = {
+    Name = format("%s_endpoint_s3", var.env_name)
+  }
+}
 
 # AWS PrivateLink(VPC endpoint(Interface)) for the others
