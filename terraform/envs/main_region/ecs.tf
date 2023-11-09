272ea008cd02b73d226457667d3c7c245e4e309b
Suzuki Takehiro <beni.web.jp@gmail.com>
2023-11-09 14:35:01 +0900

    fixed log OVERRIDE

--- terraform/envs/main_region/ecs.tf
+++ terraform/envs/main_region/ecs.tf
@@ -10,6 +10,7 @@ resource "aws_ecs_cluster" "cluster" {
   }
   configuration {
     execute_command_configuration {
+      logging = "OVERRIDE"
       log_configuration {
         cloud_watch_encryption_enabled = true
         cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs.name
