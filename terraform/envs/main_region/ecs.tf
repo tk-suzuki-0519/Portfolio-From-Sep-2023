cd614c854cc900d603205194a2723dc21fb932e7
Suzuki Takehiro <beni.web.jp@gmail.com>
2023-11-10 15:19:35 +0900

    added auto-scalling func

--- terraform/envs/main_region/ecs.tf
+++ terraform/envs/main_region/ecs.tf
@@ -65,3 +65,74 @@ resource "aws_ecs_service" "service" {
 }
 
 # Auto Scaling
+resource "aws_appautoscaling_target" "app_autoscaling_target" {
+  service_namespace  = format("%s_app_autoscaling", var.env_name)
+  resource_id        = format("service/%s_ecs_cluster/%s_ecs_service", var.env_name, var.env_name)
+  scalable_dimension = "ecs:service:DesiredCount"
+  role_arn           = data.aws_iam_role.app_autoscaling_target.arn
+  min_capacity       = 1
+  max_capacity       = 2
+}
+data "aws_iam_role" "app_autoscaling_target" {
+  name = "AWSServiceRoleForApplicationAutoScaling_ECSService"
+}
+resource "aws_appautoscaling_policy" "app_autoscaling_scale_up" {
+  name               = format("%s_name_app_autoscaling_up", var.env_name)
+  service_namespace  = format("%s_app_autoscaling_up", var.env_name)
+  resource_id        = format("service/%s_ecs_cluster/%s_ecs_service", var.env_name, var.env_name)
+  scalable_dimension = "ecs:service:DesiredCount"
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 120
+    metric_aggregation_type = "Average"
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = 1
+    }
+  }
+}
+resource "aws_appautoscaling_policy" "app_autoscaling_scale_down" {
+  name               = format("%s_name_app_autoscaling_down", var.env_name)
+  service_namespace  = format("%s_app_autoscaling_down", var.env_name)
+  resource_id        = format("service/%s_ecs_cluster/%s_ecs_service", var.env_name, var.env_name)
+  scalable_dimension = "ecs:service:DesiredCount"
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 120
+    metric_aggregation_type = "Average"
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+}
+resource "aws_cloudwatch_metric_alarm" "alarm_cpu_high" {
+  alarm_name          = format("%s_cpu_utilization_high", var.env_name)
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "70"
+  dimensions = {
+    ClusterName = format("%s_ecs_cluster", var.env_name)
+    ServiceName = format("%s_ecs_service", var.env_name)
+  }
+  alarm_actions = [aws_appautoscaling_policy.app_autoscaling_scale_up.arn]
+}
+resource "aws_cloudwatch_metric_alarm" "alarm_cpu_low" {
+  alarm_name          = format("%s_cpu_utilization_low", var.env_name)
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = 20
+  dimensions = {
+    ClusterName = format("%s_ecs_cluster", var.env_name)
+    ServiceName = format("%s_ecs_service", var.env_name)
+  }
+  alarm_actions = [aws_appautoscaling_policy.app_autoscaling_scale_down.arn]
+}
