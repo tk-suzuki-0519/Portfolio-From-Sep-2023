650de64b1b2b6adad8884e94fd103820b9d5ba34
Suzuki Takehiro <beni.web.jp@gmail.com>
2023-11-15 15:23:27 +0900

    fixed iam settings

--- terraform/envs/main_region/iam.tf
+++ terraform/envs/main_region/iam.tf
@@ -9,59 +9,8 @@ resource "aws_iam_role" "task_role" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ecs-tasks.amazonaws.com"
-      },
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-  tags = {
-    Name = format("%s_task_role", var.env_name)
-  }
-}
-# ECS Task Ececution Role
-resource "aws_iam_role" "task_execution_role" {
-  name               = format("%s_task_execution_role", var.env_name)
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ecs-tasks.amazonaws.com"
-      },
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-  tags = {
-    Name = format("%s_task_execution_role", var.env_name)
-  }
-}
-/*
-resource "aws_iam_role_policy_attachment" "AmazonECSTaskExecutionRolePolicy" {
-  role       = aws_iam_role.task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-*/
-resource "aws_iam_role_policy_attachment" "task_execution_policy_attach" {
-  role       = aws_iam_role.task_execution_role.name
-  policy_arn = aws_iam_policy.task_execution_policy.arn
-}
-resource "aws_iam_policy" "task_execution_policy" {
-  name        = format("%s_task_execution_policy", var.env_name)
-  description = format("%s_task_execution_policy", var.env_name)
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
       "Action": [
+        "ssm:GetParametersByPath",
         "ssm:GetParameters",
         "ssm:GetParameter"
       ],
@@ -93,7 +42,64 @@ resource "aws_iam_policy" "task_execution_policy" {
   ]
 }
 EOF
+  tags = {
+    Name = format("%s_task_role", var.env_name)
+  }
 }
+resource "aws_iam_policy" "task_policy" {
+  name = format("%s_task_policy", var.env_name)
+  path = "/service-role/"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Effect" : "Allow",
+        "Resource" : [
+          "*"
+        ]
+      }
+    ]
+  })
+  tags = {
+    Name = format("%s_task_policy", var.env_name)
+  }
+}
+resource "aws_iam_role_policy_attachment" "task_attachment" {
+  policy_arn = aws_iam_policy.task_policy.arn
+  role       = aws_iam_role.task_role.name
+}
+# ECS Task Ececution Role
+resource "aws_iam_role" "task_execution_role" {
+  name               = format("%s_task_execution_role", var.env_name)
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+  tags = {
+    Name = format("%s_task_execution_role", var.env_name)
+  }
+}
+resource "aws_iam_role_policy_attachment" "AmazonECSTaskExecutionRolePolicy" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 # ECSExec policy
 resource "aws_iam_policy" "ecs_exec_policy" {
   name        = format("%s_ecs_exec_policy", var.env_name)
