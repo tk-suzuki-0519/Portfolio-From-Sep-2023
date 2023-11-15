30868d7e7ea9e1cfcd95fdc6842a0ae18460af84
Suzuki Takehiro <beni.web.jp@gmail.com>
2023-11-15 14:19:37 +0900

    code transfer from '20231109_terraform_cloud_implementation_fargate' about ecs on fargate settings

---
+++ terraform/envs/main_region/ecs.tf
@@ -0,0 +1,162 @@
+# This tf file consists of ECS and Auto Scaling.
+# -----------------------------------
+# ECS
+# -----------------------------------
+resource "aws_ecs_cluster" "cluster" {
+  name = format("%s_ecs_cluster", var.env_name)
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+  configuration {
+    execute_command_configuration {
+      logging = "OVERRIDE"
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs.name
+      }
+    }
+  }
+}
+resource "aws_ecs_task_definition" "nginx_php" {
+  family = format("%s_nginx_task_definition", var.env_name)
+  container_definitions = jsonencode([
+    {
+      name      = "nginx"
+      image     = format("%s.dkr.ecr.%s.amazonaws.com/nginx:latest", var.admin_iam_id, var.main_region)
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+      dependsOn = [
+        {
+          containerName = "php"
+          condition     = "START"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs.name
+          awslogs-region        = var.main_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    },
+    {
+      name         = "php"
+      image        = format("%s.dkr.ecr.%s.amazonaws.com/portfolio-from-sep-2023-php:latest", var.admin_iam_id, var.main_region)
+      portMappings = []
+      environment  = []
+      secrets      = []
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs.name
+          awslogs-region        = var.main_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+  task_role_arn            = aws_iam_role.task_role_all.arn
+  execution_role_arn       = aws_iam_role.task_execution_role_all.arn
+  cpu                      = 512
+  memory                   = 3072 # 2048
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+}
+resource "aws_ecs_service" "service" {
+  name    = format("%s_ecs_service", var.env_name)
+  cluster = aws_ecs_cluster.cluster.arn
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    base              = 0
+    weight            = 1
+  }
+  platform_version = "1.4.0"
+  task_definition  = aws_ecs_task_definition.nginx_php.arn
+  desired_count    = 1
+  network_configuration {
+    assign_public_ip = false
+    security_groups  = [aws_security_group.fargate_sg.id]
+    subnets          = [for subnet in aws_subnet.public_subnet : subnet.id]
+  }
+  enable_execute_command = true
+  tags = {
+    Name = format("%s_ecs_service", var.env_name)
+  }
+}
+
+# Auto Scaling
+resource "aws_appautoscaling_target" "app_autoscaling_target" {
+  service_namespace  = "ecs"
+  resource_id        = format("service/%s_ecs_cluster/%s_ecs_service", var.env_name, var.env_name)
+  scalable_dimension = "ecs:service:DesiredCount"
+  role_arn           = format("arn:aws:iam::%s:role/aws-service-role/ecs.application-autoscaling.amazonaws.com/AWSServiceRoleForApplicationAutoScaling_ECSService", var.admin_iam_id)
+  min_capacity       = 1
+  max_capacity       = 2
+}
+resource "aws_appautoscaling_policy" "app_autoscaling_scale_up" {
+  name               = format("%s_name_app_autoscaling_up", var.env_name)
+  service_namespace  = "ecs"
+  resource_id        = format("service/%s_ecs_cluster/%s_ecs_service", var.env_name, var.env_name)
+  scalable_dimension = "ecs:service:DesiredCount"
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 120
+    metric_aggregation_type = "Average"
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = 1
+    }
+  }
+}
+resource "aws_appautoscaling_policy" "app_autoscaling_scale_down" {
+  name               = format("%s_name_app_autoscaling_down", var.env_name)
+  service_namespace  = "ecs"
+  resource_id        = format("service/%s_ecs_cluster/%s_ecs_service", var.env_name, var.env_name)
+  scalable_dimension = "ecs:service:DesiredCount"
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 120
+    metric_aggregation_type = "Average"
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+}
+resource "aws_cloudwatch_metric_alarm" "alarm_cpu_high" {
+  alarm_name          = format("%s_cpu_utilization_high", var.env_name)
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "70"
+  dimensions = {
+    ClusterName = format("%s_ecs_cluster", var.env_name)
+    ServiceName = format("%s_ecs_service", var.env_name)
+  }
+  alarm_actions = [aws_appautoscaling_policy.app_autoscaling_scale_up.arn]
+}
+resource "aws_cloudwatch_metric_alarm" "alarm_cpu_low" {
+  alarm_name          = format("%s_cpu_utilization_low", var.env_name)
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = 25
+  dimensions = {
+    ClusterName = format("%s_ecs_cluster", var.env_name)
+    ServiceName = format("%s_ecs_service", var.env_name)
+  }
+  alarm_actions = [aws_appautoscaling_policy.app_autoscaling_scale_down.arn]
+}
---
+++ terraform/envs/main_region/iam.tf
@@ -0,0 +1,168 @@
+# -----------------------------------
+# IAM
+# -----------------------------------
+# ECS Task Role
+resource "aws_iam_role" "task_role" {
+  name               = format("%s_task_role", var.env_name)
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+  tags = {
+    Name = format("%s_task_role", var.env_name)
+  }
+}
+# ECS Task Ececution Role
+resource "aws_iam_role" "task_execution_role" {
+  name               = format("%s_task_execution_role", var.env_name)
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+  tags = {
+    Name = format("%s_task_execution_role", var.env_name)
+  }
+}
+resource "aws_iam_role_policy_attachment" "AmazonECSTaskExecutionRolePolicy" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+/*
+resource "aws_iam_role_policy_attachment" "task_execution_policy_attach" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = aws_iam_policy.task_execution_policy.arn
+}
+resource "aws_iam_policy" "task_execution_policy" {
+  name        = format("%s_task_execution_policy", var.env_name)
+  description = format("%s_task_execution_policy", var.env_name)
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ssm:GetParameters",
+        "ssm:GetParameter"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:CompleteLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:InitiateLayerUpload",
+        "ecr:PutImage",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+*/
+# ECSExec policy
+resource "aws_iam_policy" "ecs_exec_policy" {
+  name        = format("%s_ecs_exec_policy", var.env_name)
+  description = format("%s_ecs_exec_policy", var.env_name)
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:ExecuteCommand",
+        "ecs:DescribeTasks",
+        "ssmmessages:CreateControlChannel",
+        "ssmmessages:CreateDataChannel",
+        "ssmmessages:OpenControlChannel",
+        "ssmmessages:OpenDataChannel"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_role_policy_attachment" "ecs_exec_policy_attachment" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_exec_policy.arn
+}
+resource "aws_iam_role" "ecs_autoscale_role" {
+  name               = format("%s_ecs_autoscale_role", var.env_name)
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "application-autoscaling.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_policy" "ecs_autoscale_role" {
+  name   = format("%s_ecs_autoscale_policy", var.env_name)
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Action": [
+            "ecs:DescribeServices",
+            "ecs:UpdateService",
+            "cloudwatch:PutMetricAlarm",
+            "cloudwatch:DescribeAlarms",
+            "cloudwatch:DeleteAlarms"
+        ],
+        "Resource": [
+            "*"
+        ],
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_role_policy_attachment" "ecs_autoscale_role_attach" {
+  role       = aws_iam_role.ecs_autoscale_role.name
+  policy_arn = aws_iam_policy.ecs_autoscale_role.arn
+}
