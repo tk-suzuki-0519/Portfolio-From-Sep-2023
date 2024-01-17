cd55a8cd4f0f8d28a16726ef7075e80496739c08
Suzuki Takehiro <beni.web.jp@gmail.com>
2024-01-17 11:05:23 +0900

    added AmazonEC2ContainerRegistryReadOnly policy

--- terraform/envs/main_region/iam.tf
+++ terraform/envs/main_region/iam.tf
@@ -77,9 +77,9 @@ resource "aws_iam_role_policy_attachment" "task_attachment" {
   role       = aws_iam_role.task_role.name
   policy_arn = aws_iam_policy.task_policy.arn
 }
-# ECS Task Ececution Role
+# ECS Task Execution Role
 resource "aws_iam_role" "task_execution_role" {
-  name               = format("%s_task_execution_role", var.env_name)
+  name               = format("%s_task_execution_role", var.env_name) #下記で、AmazonEC2ContainerRegistryReadOnlyを付与
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -90,7 +90,25 @@ resource "aws_iam_role" "task_execution_role" {
         "Service": "ecs-tasks.amazonaws.com"
       },
       "Effect": "Allow"
-    }
+    },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:GetRepositoryPolicy",
+                "ecr:DescribeRepositories",
+                "ecr:ListImages",
+                "ecr:DescribeImages",
+                "ecr:BatchGetImage",
+                "ecr:GetLifecyclePolicy",
+                "ecr:GetLifecyclePolicyPreview",
+                "ecr:ListTagsForResource",
+                "ecr:DescribeImageScanFindings"
+            ],
+            "Resource": "*"
+        }
   ]
 }
 EOF
