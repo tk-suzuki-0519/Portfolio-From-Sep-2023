d31f5c672cb3a6d8ee2de6385ad2174e89ece57b
Suzuki Takehiro <beni.web.jp@gmail.com>
2024-01-23 10:53:01 +0900

    modified aws_iam_role 4

--- terraform/envs/main_region/iam.tf
+++ terraform/envs/main_region/iam.tf
@@ -8,21 +8,12 @@ resource "aws_iam_role" "task_role" {
     "Version" : "2012-10-17",
     "Statement" : [
       {
-        "Action" : "*",
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "ecs-tasks.amazonaws.com"
-        }
-      }
-      /*
-      {
         "Action" : "sts:AssumeRole",
         "Effect" : "Allow",
         "Principal" : {
           "Service" : "ecs-tasks.amazonaws.com"
         }
       }
-      */
     ]
   })
   tags = {
@@ -93,7 +84,7 @@ resource "aws_iam_role" "task_execution_role" {
     "Version" : "2012-10-17",
     "Statement" : [
       {
-        "Action" : "*",
+        "Action" : "sts:AssumeRole",
         "Effect" : "Allow",
         "Principal" : {
           "Service" : "ecs-tasks.amazonaws.com"
