8b44d58770399fb678eacb52f335d144cdb4b375
Suzuki Takehiro <beni.web.jp@gmail.com>
2024-02-11 10:47:01 +0900

    added ecs exec log settings

--- terraform/envs/main_region/cloudwatch_logs.tf
+++ terraform/envs/main_region/cloudwatch_logs.tf
@@ -17,3 +17,7 @@ resource "aws_cloudwatch_log_group" "ecs" {
   name              = format("/aws/ecs/%s", var.env_name)
   retention_in_days = 90
 }
+resource "aws_cloudwatch_log_group" "ecs_exec" {
+  name              = format("/aws/ecs_exec/%s", var.env_name)
+  retention_in_days = 90
+}
