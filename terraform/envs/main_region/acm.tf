90b4c8791d9fe5f30105d57d2e3d9173c242e261
Suzuki Takehiro <beni.web.jp@gmail.com>
2024-02-14 05:35:37 +0900

    created first acm and route53

---
+++ terraform/envs/main_region/acm.tf
@@ -0,0 +1,34 @@
+# -----------------------------------
+# acm
+# -----------------------------------
+resource "aws_acm_certificate" "main_region_cert" {
+  domain_name = var.main_domain
+  subject_alternative_names = [format("www.%s", var.main_domain)]
+  validation_method = "DNS"
+  tags = {
+    Name = format("%s_cert", var.main_domain)
+  }
+  # デフォルトではリソースが削除されてから作成されるが、証明書に関してはロードバランサーで使用中なので、作成してから削除させる
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_acm_certificate_validation" "cert_valid" {
+  certificate_arn = aws_acm_certificate.main_region_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_valid : record.fqdn]
+}
+resource "aws_route53_record" "cert_valid" {
+  for_each = {
+    for dvo in aws_acm_certificate.main_region_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+  allow_overwrite = true
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 60
+  type    = each.value.type
+  zone_id = data.aws_route53_zone.main_domain.id
+}
--- terraform/envs/main_region/route53.tf
+++ terraform/envs/main_region/route53.tf
@@ -1,4 +1,26 @@
 # -----------------------------------
 # route 53
 # -----------------------------------
-# my-fqdn
+data "aws_route53_zone" "main_domain" {
+  name = var.main_domain
+}
+resource "aws_route53_zone" "sub_domain" {
+  name = format("www.%s", var.main_domain)
+}
+resource "aws_route53_record" "sub-ns" {
+  zone_id = aws_route53_zone.main_domain.zone_id
+  name    = format("www.%s", var.main_domain)
+  type    = "NS"
+  ttl     = "300"
+  records = aws_route53_zone.sub-ns.name_servers
+}
+resource "aws_route53_record" "sub-a" {
+  zone_id = aws_route53_zone.sub-ns.zone_id
+  name    = aws_route53_zone.sub-ns.name
+  type    = "A"
+  alias {
+    name                   = aws_lb.alb.dns_name
+    zone_id                = aws_lb.alb.zone_id
+    evaluate_target_health = true
+  }
+}
