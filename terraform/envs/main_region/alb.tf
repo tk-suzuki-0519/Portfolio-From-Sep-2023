87097edded599e1e66d7d36109f3c795b61c86c1
Suzuki Takehiro <beni.web.jp@gmail.com>
2024-02-14 13:32:37 +0900

    fixed https listener on alb

--- terraform/envs/main_region/alb.tf
+++ terraform/envs/main_region/alb.tf
@@ -71,7 +71,8 @@ resource "aws_lb_listener" "https_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "443"
   protocol          = "HTTPS"
-
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = data.aws_acm_certificate.main_region_cert.arn
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alb_tg.arn
